33f6e28474c5f39f7693a343bb7dc8d64a27ca25
Chris Socha <cschlumb@thoughtworks.com>
2015-09-16 14:22:51 +0100

    Use requiresjs with jquery

--- src/main/resources/assets/js/smart-mirror.js
+++ src/main/resources/assets/js/smart-mirror.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+'use strict';
+
+require.config({
+    baseUrl: 'js/',
+    paths: {
+        jquery: 'vendor/jquery/dist/jquery'
+    }
+});
+
+define([
+    'jquery'
+], function ($) {
+
+    console.log($('body'));
+
+});
